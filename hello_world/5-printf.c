cfbf547cf0842393848acc887f42b50dced8766b
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-21 13:07:46 -0400

    fix

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a statement about art,
+ * demonstrating the beauty and depth of artistic expression.
+ *
+ * Return: Always returns 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
