4e4d547456103b34a09cb186621ad0ef94bbdb16
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-21 12:24:27 -0400

    error

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a specific string
+ * to the standard output, illustrating the concept
+ * that programming involves creativity and structure.
+ *
+ * Return: Always returns 0 (Success)
+ */
+
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"  Programming is like building a multilingual puzzle");
 return (0);
 }
