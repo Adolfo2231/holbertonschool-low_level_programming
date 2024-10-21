c1dafc58a4089bb567566b6378a43fdf5d655cbc
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-21 12:53:30 -0400

    fx

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point of the program
  *
@@ -8,8 +7,7 @@
  * that programming involves creativity and structure.
  *
  * Return: Always returns 0 (Success)
- */
-
+ */:
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
