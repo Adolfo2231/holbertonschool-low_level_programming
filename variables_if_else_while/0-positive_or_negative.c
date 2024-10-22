82c5ec38bb8f5b85cfc19c6dc73651bc7cbb0afe
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-22 09:41:35 -0400

    add time.h library

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
@@ -10,7 +10,9 @@
  */
 int main(void)
 {
-int n = rand();
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
 printf("%d is positive\n", n);
