794dcadb90e73fcfe5398a59538b975d3e9f3b98
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-21 22:56:48 -0400

    Positive or negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+if (n > 0)
+{
+	printf("is positive\n");
+}
+else if (n == 0)
+{
+	printf("is zero\n");
+}
+else if (n < 0)
+{
+	printf("is negative\n");
+}
+	return (0);
+}
