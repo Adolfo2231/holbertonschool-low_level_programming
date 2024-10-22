57f4861cfb83fdef26983b0f0943efd7eee6a395
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-21 23:03:59 -0400

    add comments

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program assigns a random number to the variable n
+ * and prints whether n is positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
+int n;
 if (n > 0)
 {
 	printf("is positive\n");
