16207b9ade9bf4f2dfad88a55b6a97a4564b041f
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-22 14:02:08 -0400

    Betty documantation

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 /**
- * Print all single-digit numbers from 0 to 9
- * with loop while
- * using putchar
-*/
+ * main - Prints single-digit numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-        int n = 0;
+	int n = 0;
 
-        while (n < 10)
-        {
-                putchar(n + '0');
-                n++;
-        }
-        putchar('\n');
-        return (0);
+	while (n < 10)
+	{
+		putchar(n + '0');
+		n++;
+	}
+	putchar('\n');
+	return (0);
 }
