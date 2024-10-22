f6672059dae4d523e3bb65991414cb79b57b65af
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-22 13:33:56 -0400

    prints numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Print all single-digit numbers from 0 to 9.
+*/
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%d", n);
+		n++;
+	}
+	printf("\n");
+}
+	
