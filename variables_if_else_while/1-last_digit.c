cb3cc00685703b24f33c817eb940fc6d569c8348
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-23 06:53:38 -0400

    hi

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/*
- * Main - Entry Point
+
+/**
+ * main - Entry point
  *
- * Return: Always 0
+ * Description: Prints the last digit of a random number stored in n
+ * and evaluates it as per the conditions:
+ * - If the last digit is greater than 5
+ * - If the last digit is 0
+ * - If the last digit is less than 6 and not 0
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	int last, n;
