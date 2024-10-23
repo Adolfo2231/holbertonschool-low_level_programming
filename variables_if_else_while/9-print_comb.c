1e2962945acd67ced1db8a062b4eb7a2a98934cc
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-23 00:09:11 -0400

    fix

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Prints single-digit numbers from 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int n = 0;
+	int s = 34;
+        while (n < 10)
+        {
+                putchar(n + '0');
+
+		if ( n != 9)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+		else
+	       	{
+			
+		}
+                n++;
+        }
+        putchar('$');
+	return (0);
+}
