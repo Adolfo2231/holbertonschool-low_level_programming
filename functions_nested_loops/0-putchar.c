2f621b94ab19327d2abdd1a9e96112611d59b88a
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-24 10:46:29 -0400

    fix prototype in the files main.h

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,12 @@
-#include <stdio.h>
 #include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints "_putchar"
+ * followed by a new line.
+ * Return: 0 (Success)
+ */
 
 int main(void)
 {
@@ -12,4 +19,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
+	return (0);
 }
