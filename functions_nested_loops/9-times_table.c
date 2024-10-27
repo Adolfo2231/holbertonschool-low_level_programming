5b4c275b7e35a586f455fca8d79f4ebf57d1f2a8
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-27 15:50:52 -0400

    fic spacing

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -8,34 +8,34 @@
  * Each number is printed in its own column, and proper formatting is applied
  * to ensure alignment.
  */
-
 void times_table(void)
 {
-	int a, b;
+	int a, b, product;
 
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			int product = a * b;
+			product = a * b;
 
 			if (b != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
 			}
-			if (product < 10)
+
+			if (product < 10 && b != 0)
 			{
 				_putchar(' ');
-				_putchar(product + '0');
-
 			}
-			else
+
+			if (product >= 10)
 			{
 				_putchar((product / 10) + '0');
-				_putchar((product % 10) + '0');
 			}
+			_putchar((product % 10) + '0');
 		}
+
 		_putchar('\n');
 	}
 }
