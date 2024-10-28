64bd2ab879363bcf7887e2cf49336919bda30a09
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-28 11:34:14 -0400

    fix

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,12 @@ void print_most_numbers(void)
 {
 	int n = 0;
 
-	while ((n != 2 || n != 4) && n < 10)
+	while (n < 10)
 	{
-		_putchar(n + '0');
+		if (n != 2 && n != 4)
+		{
+			_putchar(n + '0');
+		}
 		n++;
 	}
 	_putchar('\n');
