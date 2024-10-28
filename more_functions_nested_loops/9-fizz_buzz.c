c9fa3ddc549669443ad43844601bfa4e66197694
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-28 14:51:14 -0400

    instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz.

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * multiplo - Checks if a number is a multiple of 3 or 5.
+ * @n: The number to check.
+ *
+ *
+ * This function prints "Fizz" if the number is a multiple of 3,
+ * "Buzz" if it is a multiple of 5, "FizzBuzz" if it is a
+ * multiple of both, or the number itself if neither.
+ */
+
+void multiplo(int n)
+{
+	int multiplo3 = n % 3;
+	int multiplo5 = n % 5;
+
+	if (multiplo3 == 0)
+	{
+		printf("Fizz ");
+	}
+	else if (multiplo5 == 0)
+	{
+		printf("Buzzz ");
+	}
+	else if (multiplo3 == 0 && multiplo5 == 0)
+	{
+		printf("FizzBuzz ");
+	}
+	else
+	{
+		printf("%d ", n);
+	}
+}
+
+/**
+ * main - Entry point of the program.
+ *
+ * This function iterates through numbers from 1 to 100,
+ * calling the multiplo function to print the appropriate
+ * result for each number.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int num = 1;
+
+	while (num <= 100)
+	{
+		multiplo(num);
+		num++;
+	}
+	printf("\n");
+	return (0);
+}
