2fdee5ecf7c0f6052f93552e19ee452fdd765457
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-28 11:07:48 -0400

    3. The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * print_numbers - Prints the numbers from 0 to 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		_putchar(n);
+	}
+}
