6871512e0ef9eb9e724c4b57f2703d4fde1e57c2
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-30 10:03:31 -0400

    fix

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -14,18 +14,23 @@ void print_triangle(int size)
 	int j;
 	int i;
 
-	for (i = 0; i < size; i++)
+	if (size <= 0)
 	{
-		for (j = 0; j < size - i - 1; j++)
-		{
-			_putchar(' ');
-		}
-		for (j = 0; j < i + 1; j++)
-		{
-			_putchar('#');
-		}
 		_putchar('\n');
-
 	}
-
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size - i - 1; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < i + 1; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
