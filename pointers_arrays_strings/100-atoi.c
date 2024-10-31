b7d57c03fc44a7a17f504880945f0cbf53820ddc
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-10-31 09:57:29 -0400

    Write a function that convert a string to an integer.

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The integer representation of the string.
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+	int overflow_check;
+
+	/* Skip any leading characters until finding a sign or digit */
+	while (*s && (*s < '0' || *s > '9'))
+	{
+		if (*s == '-')
+			sign = -sign;
+		s++;
+	}
+
+	/* Convert each digit to an integer */
+	while (*s >= '0' && *s <= '9')
+	{
+		/* Check for overflow before updating result */
+		overflow_check = result;
+		result = result * 10 + (*s - '0');
+
+		if (overflow_check != result / 10)
+			return (sign == 1 ? 2147483647 : -2147483648);
+		s++;
+	}
+	return (result * sign);
+}
