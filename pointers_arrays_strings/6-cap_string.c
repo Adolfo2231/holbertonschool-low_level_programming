cbe092b14dfae5380efc51c9688804bbb657a31f
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-11-01 14:26:31 -0400

    fix4

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h" /* Include the header file with function prototypes */
 
 /**
- * cap_string - Capitalizes all words in a string.
+ * cap_string - Capitalizes the first letter of each word in a string.
  * @str: Pointer to the string to be modified.
  *
  * Description: This function capitalizes the first letter of each word
@@ -12,44 +12,40 @@
  */
 char *cap_string(char *str)
 {
-	int i; /* Index variable for iterating through the string */
+    int i; /* Index variable for iterating through the string */
+    int capitalize_next; /* Flag to indicate if next character should be capitalized */
+    char separators[] = " \t\n,;.!?\"(){}"; /* List of separators */
 
-	int capitalize_next;
-	/* Flag to indicate if next character should be capitalized */
+    i = 0; /* Initialize index */
+    capitalize_next = 1; /* Set to true for the first character */
 
-	int j;
-	char separators[] = "\t\n,;.!?\"(){}"; /* List of separators */
+    /* Iterate through each character of the string until the null terminator */
+    while (str[i] != '\0') /* Check for the end of the string */
+    {
+        /* Check if we need to capitalize the current character */
+        if (capitalize_next && str[i] >= 'a' && str[i] <= 'z') /* Only lowercase letters */
+        {
+            str[i] = str[i] - ('a' - 'A'); /* Convert to uppercase */
+            capitalize_next = 0; /* Reset flag after capitalizing */
+        }
+        /* Check if current character is a separator */
+        else
+        {
+            int j; /* Variable to iterate through the separators */
 
-	i = 0; /* Initialize index */
-	capitalize_next = 1; /* Set to true for the first character */
+            /* Loop through each separator character */
+            for (j = 0; separators[j] != '\0'; j++)
+            {
+                if (str[i] == separators[j]) /* If character is a separator */
+                {
+                    capitalize_next = 1; /* Set flag to capitalize next letter */
+		    break; /* Exit the separator check loop */
+                }
+            }
+        }
 
-	/* Iterate through each character of the string until the null terminator */
-	while (str[i] != '\0') /* Check for the end of the string */
-	{
-		/* Check if the current character is a separator */
-		for (j = 0; separators[j] != '\0'; j++) /* Loop through separators */
-		{
-			if (str[i] == separators[j]) /* If a separator is found */
-			{
-				capitalize_next = 1; /* Next character should be capitalized */
-				break; /* Exit the loop for separators */
-			}
-		}
+        i++; /* Move to the next character in the string */
+    }
 
-		/* Capitalize the current character if the flag is set */
-		if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
-			/* Check if character is lowercase */
-		{
-			str[i] = str[i] - ('a' - 'A'); /* Convert to uppercase */
-		}
-
-		/* Reset the flag */
-		else if (str[i] != ' ') /* If current character is not a space */
-		{
-			capitalize_next = 0; /* Next character should not be capitalized */
-		}
-
-		i++; /* Move to the next character in the string */
-	}
-	return (str); /* Return the pointer to the modified string */
+    return (str); /* Return the pointer to the modified string */
 }
