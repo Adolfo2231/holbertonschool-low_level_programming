3ea5b0e40ed8d6b1f6e4b4df31d3ba3d5cec72df
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-11-04 12:15:15 -0400

    fix

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -6,19 +6,28 @@
  * @c: The character to find.
  *
  * Return: A pointer to the first occurrence of `c` in `s`,
- *
+ * or NULL if the character is not found.
  */
-
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+    int i = 0;
 
-	while (s[i] != '\0')
-	{
-		if (s[i] == c)
-		{
-			return (&s[i]);
-		}
-		i++;
-	}
+    /* Loop through each character in the string */
+    while (s[i] != '\0')
+    {
+        if (s[i] == c)
+        {
+            return (&s[i]); /* Return address of the first occurrence */
+        }
+        i++;
+    }
+
+    /* Check for the null terminator, if c is '\0' */
+    if (c == '\0')
+    {
+        return (&s[i]); /* Return address of the null terminator */
+    }
+
+    return (s);
 }
+
