183c644ac8a3fcfe2caf4f5297decb0c35380ad3
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-11-06 15:05:13 -0400

    fix betty style

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -6,11 +6,10 @@
  */
 void _print_rev_recursion(char *s)
 {
-    if (*s == '\0') /* Base case: end of string */
-    {
-        return;
-    }
-
-    _print_rev_recursion(s + 1); /* Recursive call with the next character */
-    _putchar(*s); /* Print the current character after recursive calls */
+	if (*s == '\0') /* Base case: end of string */
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1); /* Recursive call with the next character */
+	_putchar(*s); /* Print the current character after recursive calls */
 }
