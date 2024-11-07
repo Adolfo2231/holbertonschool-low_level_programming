4085796e48c65a3472373f8638f75221e96791b8
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-11-07 10:23:55 -0400

    fix counter

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,10 +1,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ * @argc: Argument count
+ * @argv: Array of argument strings (unused in this program)
+ *
+ * Description: Prints the number of arguments passed to the program,
+ * excluding the program's name itself.
+ * Return: 0 on success
+ */
+
 int main(int argc, char *argv[])
 {
 	(void)argv; /* To ignore the error unused variables*/
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
