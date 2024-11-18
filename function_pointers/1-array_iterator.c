afd80809aaaf54eca82138af3b8bd6e39e5c97c0
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-11-18 14:23:28 -0400

    add betty documentation and style

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,12 +1,20 @@
-#include <stddef.h>
 #include "function_pointers.h"
+
+/**
+ * array_iterator - Itera en cada uno de los elementos del array
+ * a la vez que lo envia al pointer to function
+ *
+ * @array: Pointer to the array
+ * @size: size of the elements of the array
+ * @action: function to check
+ */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    size_t i; /* is an unsigned integer type same like size*/
+	size_t i; /* is an unsigned integer type same like size*/
 
-    for (i = 0; i < size; i++)
-    {
-        action(array[i]);
-    }
-} 
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
