322e9243edbb35a1697f0d1319991ad44b53b8af
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-11-18 11:47:08 -0400

    add betty comments and style

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,12 +1,13 @@
 #include "function_pointers.h"
 
 /**
- * 
  * print_name - Take to parameter a name and function to pointer
- * 
+ *
+ * @name: Receive a pointer string
+ * @f: function callback, receive a funtion pointer
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+	f(name);
 }
