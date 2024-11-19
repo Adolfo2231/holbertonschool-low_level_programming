70315b43ba48afa48ceaeb2cb07d0ae3e7752964
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-11-19 11:35:56 -0400

    fix the limits of the if statments

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -30,19 +30,11 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	if (argv[2][0] == '/' && (num1 == 0 || num2 == 0))
+	if (argv[2][0] == '/' || (argv[2][0] == '%' && (num1 == 0 || num2 == 0)))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
-	if (argv[2][0] == '%' && (num1 == 0 || num2 == 0))
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-
 	result = operation(num1, num2);
 	printf("%d\n", result);
 
