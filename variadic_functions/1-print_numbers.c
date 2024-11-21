c22098b0716b1c14a6f5a68a00d671e21078e32a
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-11-21 10:53:04 -0400

    Write a function that prints numbers, followed by a new line.

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -4,28 +4,37 @@
 #include <stdio.h>
 #include <ctype.h>
 
-    void print_numbers(const char *separator, const unsigned int n, ...)
+/**
+ * print_numbers - receive a number of elements and prints them
+ *
+ * @separator: char elements to separate number elements
+ * @n: number of elements
+ *
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    unsigned int i;
+	unsigned int i;
 
-    va_list args;
-    va_start(args, n);
+	va_list args;
 
-    if (separator == NULL)
-    {
-        printf("\n");
-    }
+	va_start(args, n);
 
-    for (i = 0; i < n; i++)
-    {
-        if (i == (n -1))
-        {
-            printf("%i", va_arg(args, int));
-        }
-        else
-        {
-        printf("%i%s", va_arg(args, int), separator);
-        }
-    }
-    va_end(args);
+	if (separator == NULL)
+	{
+		printf("\n");
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+		{
+			printf("%i", va_arg(args, int));
+		}
+		else
+		{
+			printf("%i%s", va_arg(args, int), separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
 }
