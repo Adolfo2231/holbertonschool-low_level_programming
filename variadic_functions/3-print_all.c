94b0de3f2da4235f507d1680043c42e975608aa2
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-11-21 14:03:50 -0400

    fix problem with spaces

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,50 +1,46 @@
 #include "variadic_functions.h"
-#include "stdarg.h"
+#include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * print_all - Prints arguments of different types based on a format string
+ * @format: A string specifying the argument types ('c', 'i', 'f', 's')
+ */
 void print_all(const char *const format, ...)
 {
-    int i = 0;
-    char separator = '\0';
-    char *ptr;
+	int i = 0;
+	char *ptr, *sep = "";
 
-    va_list args;
-    va_start(args, format);
+	va_list args;
 
-    while (format && format[i] != '\0')
-    {
-        switch (format[i])
-        {
-        case 'c':
-            printf("%c ", separator);
-            printf("%c", va_arg(args, int));
-            break;
-        case 'i':
-            printf("%c ", separator);
-            printf("%d", va_arg(args, int));
-            break;
-        case 'f':
-            printf("%c ", separator);
-            printf("%f", va_arg(args, double));
-            break;
-        case 's':
-            printf("%c ", separator);
-            ptr = va_arg(args, char *);
-            if (ptr == NULL)
-            {
-                printf("(nil)");
-            }
-            else
-            {
-                printf("%s ", ptr);
-            }
-            break;
-        default:
-            break;
-        }
-        separator = ',';
-        i++;
-    }
-    printf(("\n"));
-    va_end(args);
+	va_start(args, format);
+
+	while (format && format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				break;
+			case 's':
+				ptr = va_arg(args, char *);
+				if (ptr == NULL)
+					ptr = "(nil)";
+				printf("%s%s", sep, ptr);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		sep = ", "; /*Set separator after the first valid element*/
+		i++;
+	}
+	printf("\n");
+	va_end(args);
 }
