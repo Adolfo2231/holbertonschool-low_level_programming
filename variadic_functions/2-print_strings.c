5da4ac864fb4255a48d9719fbbf4a4698bc8cb5b
Adolfo2231 <adolfo.rodriguez2231@gmail.com>
2024-11-21 13:37:53 -0400

    add betty style and description

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -4,44 +4,48 @@
 #include <stdio.h>
 #include <ctype.h>
 
+/**
+ * print_strings - print strings
+ *
+ * @separator: char
+ * @n: number of elements
+ *
+ *
+ */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    unsigned int i;
-    va_list args;
-    char *str;
+	unsigned int i;
+	char *str;
+	va_list args;
 
-    va_start(args, n);
+	va_start(args, n);
 
-    if (separator == NULL)
-    {
-        printf("\n");
-    }
+	if (separator == NULL)
+	{
+		printf("\n");
+	}
 
-    if (args == NULL)
-    {
-        printf("nil");
-    }
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char *);
 
-    for (i = 0; i < n; i++)
-    {
-        str = va_arg(args, char *);
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
 
-        if (str == NULL)
-        {
-            printf("(nil)");
-        }
-        else
-        {
+			printf("%s", str);
+		}
 
-            printf("%s", str);
-        }
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
 
-        if (separator != NULL && i < n - 1)
-        {
-            printf("%s", separator);
-        }
-    }
-
-    va_end(args);
-    printf("\n");
+	va_end(args);
+	printf("\n");
 }
